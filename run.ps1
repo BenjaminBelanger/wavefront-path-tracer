ccd91394516a8f24316415b54ce7f0e37a549959
BenjaminBelanger <benjamin-belanger@hotmail.com>
2026-05-07 00:07:34 -0700

    Add run.ps1 build-and-run script

---
+++ run.ps1
@@ -0,0 +1,7 @@
+$ScriptDir = $PSScriptRoot
+
+cmake -S $ScriptDir -B "$ScriptDir/build" -G "Visual Studio 17 2022" -A x64
+
+cmake --build "$ScriptDir/build" --config Release --target wavefront-path-tracer
+
+& "$ScriptDir/build/bin/Release/wavefront-path-tracer.exe"
