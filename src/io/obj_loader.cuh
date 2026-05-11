a34f65838d5aad5fff0cd9c40d192d5b0beb1b14
BenjaminBelanger <benjamin-belanger@hotmail.com>
2026-05-11 16:04:20 -0700

    Add OBJ mesh loading support

---
+++ src/io/obj_loader.cuh
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "../app/scene.cuh"
+#include <string>
+
+namespace wpt
+{
+
+    struct ObjLoadOptions
+    {
+        float3 translation = {0, 0, 0};
+        float3 rotation = {0, 0, 0};
+        float scale = 1.0f;
+        bool recalculate_normals = false;
+    };
+
+    bool load_obj(Scene &scene, const std::string &filepath,
+                  const ObjLoadOptions &opts = {});
+
+}
